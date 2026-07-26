161948d2312926f0594779498e81e16eb6440152
Maria Morales <MariaElizabeth.Morales@alu.frt.utn.edu.ar>
2026-07-26 02:25:37 -0300

    agregar borrado logico y filtro global

--- Dsw2026Tpi.Data/Configurations/DoctorConfiguration.cs
+++ Dsw2026Tpi.Data/Configurations/DoctorConfiguration.cs
@@ -9,5 +9,24 @@ public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
     public void Configure(EntityTypeBuilder<Doctor> builder)
     {
         builder.ToTable("Doctors");
+
+        builder.Property(d => d.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(d => d.LicenseNumber)
+            .IsRequired()
+            .HasMaxLength(20);
+
+        builder.HasIndex(d => d.LicenseNumber)
+            .IsUnique()
+            .HasFilter("[IsActive] = 1");
+
+        builder.HasOne(d => d.Speciality)
+            .WithMany()
+            .HasForeignKey(d => d.SpecialityId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasQueryFilter(d => d.IsActive);
     }
 }
