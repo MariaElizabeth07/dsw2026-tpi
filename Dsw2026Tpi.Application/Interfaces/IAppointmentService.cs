b22865bbedbe9c196ccdf6bfb7377aaad0ec0d6b
Maria Morales <MariaElizabeth.Morales@alu.frt.utn.edu.ar>
2026-07-27 11:45:33 -0300

    definir dtos e interfaz de servicio para la gestion de turnos

---
+++ Dsw2026Tpi.Application/Interfaces/IAppointmentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Dsw2026Tpi.Application.Dtos;
+
+namespace Dsw2026Tpi.Application.Interfaces;
+
+public interface IAppointmentService
+{
+    Task<AppointmentModel.Response> Create(AppointmentModel.Request request, string authenticatedEmail);
+    Task<IReadOnlyCollection<AppointmentModel.Response>> GetByPatient(long dni, string authenticatedEmail);
+    Task Cancel(Guid id, string authenticatedEmail);
+}
